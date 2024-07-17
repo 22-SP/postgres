7a1dfc94f9c5d5354fe1652ce3f1500530a4c500
Steven Poole <stevenpoole234@gmail.com>
2024-07-17 16:45:19 +0000

    fixed the problems with the files throwing errors

--- sql-expression.py
+++ sql-expression.py
@@ -40,12 +40,29 @@ track_table = Table(
 # Making the connection
 with db.connect() as connection: 
     #Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
 
     # Query 2
     # Select only the "Name" column from the "Artist" table
     # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
     
+    # Query 3
+    # Select only 'Queen'from the artist table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4
+    # Select only 'ArtistById' #51 from the "Artist Table"
+    # select_query = artist_table_select()where(album_table.c.ArtistId == 51)
+
+    # Query 5
+    # Select only the albums with 'ArtistId' #51 on the "Album Table"
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+
+    # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+
+
     results = connection.execute(select_query)
     for result in results:
         print(result)
